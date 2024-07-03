5e6058308c38acd0f4dc4a7d75e6ee31b215cfd1
Tomas <tomas.avola@coinfabrik.com>
2024-07-03 09:28:04 -0300

    Hardhat files and ethervault contract

---
+++ contracts/Lock.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+/** Inspired by https://ethernaut.openzeppelin.com/level/10 */
+
+contract EtherVault {
+    using Math for uint256;
+
+    mapping(address => uint256) public balances;
+
+    function donate(address _to) public payable {
+        (bool success, uint256 result) = balances[_to].tryAdd(msg.value);
+        require(success, "Invalid Math operation.");
+        balances[_to] = result;
+    }
+
+    function balanceOf(address _who) public view returns (uint256 balance) {
+        return balances[_who];
+    }
+
+    function withdraw(uint256 _amount) public {
+        if (balances[msg.sender] >= _amount) {
+            (bool result,) = msg.sender.call{value: _amount}("");
+            if (result) {
+                _amount;
+            }
+            balances[msg.sender] -= _amount;
+        }
+    }
+
+    receive() external payable {}
+}
